80bd75943f21fa2c5b07d2eb30bf158dd7c18d49
Robin Fabre <rf.school@icloud.com>
2016-06-21 15:52:24 +0200

    fiche adherent termine

--- app/js/adherents/AdherentsController.js
+++ app/js/adherents/AdherentsController.js
@@ -2,44 +2,38 @@ angular.module('adherentsModule')
     .controller('AdherentsController', function($scope, adherentsService) {
 
         $scope.adherents = [];
-        adherentsService.getAdherents().then(function(param){
-                    $scope.adherents = param;
-                });
+        adherentsService.getAdherents().then(function(param) {
+            $scope.adherents = param;
+        });
 
+        $scope.calculAge = function() {
+            var dateNaiss = $scope.dateNaissance;
+            if (dateNaiss !== undefined) {
+                dateNaiss = dateNaiss.split("/");
 
+                var dateNaissFormat = new Date(dateNaiss[1] + ' ' + dateNaiss[0] + ' ' + dateNaiss[2]);
 
+                var ageDifMs = Date.now() - dateNaissFormat.getTime();
+                var ageDate = new Date(ageDifMs);
+                var ageFinal = Math.abs(ageDate.getUTCFullYear() - 1970);
+                $scope.age = ageFinal;
 
+            }
 
-                $scope.finCotisation = function(){
+        }
 
+        $scope.finCotisation = function() {
+            var dateDeb = $scope.datePaiementCotisation
+            if (dateDeb !== undefined) {
+                dateDeb = dateDeb.split("/");
 
+                var dateDebFormat = new Date(dateDeb[1] + ' ' + dateDeb[0] + ' ' + dateDeb[2]);
 
-                    var dateDeb = $scope.datePaiementCotisation
-                    if (dateDeb !== undefined){
-                        dateDeb = dateDeb.split("-");
-                        console.log(dateDeb);
+                var dateFinAbonnement = new Date(dateDebFormat.setFullYear(dateDebFormat.getFullYear() + 1));
 
-                        var dateFin = new Date(dateDeb[1] +' '+ dateDeb[0] +' '+ dateDeb[2] );
-                        $scope.dateFinAbonnement =
-                        console.log($scope.dateFinAbonnement);
+                $scope.dateFinAbonnement = dateFinAbonnement.toLocaleDateString();
 
-                    }
-
-
-
-
-
-
-
-                }
-
-
-
-                    return dateFin.setFullYear(dateDebut.getFullYear()+1);
-                    console.log(dateFin);
-
-
-
-
+            }
+        }
 
     });
