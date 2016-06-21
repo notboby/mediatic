5185da8fdad08c54de722c8a35f8a33c430511f6
fredgit2016 <le-gac.frederic@laposte.net>
2016-06-21 10:23:25 +0200

    creation fiche media -modification MediasController

--- app/js/medias/MediasController.js
+++ app/js/medias/MediasController.js
@@ -12,9 +12,36 @@ angular
 			console.error('Erreur de chargement des medias');
 		});
 		
-		//$scope.
+		
+		
+		$scope.ajout = function(){
+			
+			console.log("test fiche medias");
+			
+			var myUrl = 'http://192.168.10.12:8090/resource/media.creation'
+			$http.post(myUrl,
+					{titre:$scope.titre, auteur:$scope.auteur, type:$scope.type} 
+					).then(function(response){
 
+						console.log("OK§§§§!!!!!");
+						console.log(response.data);
+						
+					}, function(response){
+						console.error('Erreur de connexion', response);
+						$http.defaults.headers.common.authorization = 'Basic ';
+					});
+	}
 		
 		
-		
-	});
+});
+
+
+angular.module('showcase.rowClickEvent', ['datatables'])
+.controller('RowClickEventCtrl', RowClickEventCtrl);
+
+function RowClickEventCtrl($scope, DTOptionsBuilder, DTColumnBuilder) {
+	console.log("O sa marche");
+
+}
+
+
